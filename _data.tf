4542a4dade9dc8ede4cc377f5a29496d716c18e5
vishal <vishal.bhogate07@gmail.com>
2020-07-13 14:37:14 +1000

    Db monitoring

--- _data.tf
+++ _data.tf
@@ -1,1 +1,7 @@
-data "type" "name" {}
+data aws_iam_account_alias "current" {
+
+}
+
+data aws_db_instance "current" {
+  db_instance_identifier = var.identifier
+}
--- _variable.tf
+++ _variable.tf
@@ -1,4 +1,13 @@
-variable "name" {
-  default     = "" 
-  description = ""
+variable "identifier" {
+  default     = ""
+  description = "RDS database instance identifier"
+}
+
+variable "instance_class" {
+  description = "RDS database instance class"
+}
+
+variable "alarm_sns_topics" {
+  default     = []
+  description = "Alarm topics to create and alert on RDS instance metrics"
 }
